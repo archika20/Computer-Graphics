d1c16c7dafff40d9f25d36237a3e5136f73d6b1f
Archika Vyas <53368347+archika20@users.noreply.github.com>
2019-08-15 20:18:15 +0530

    Create Line.c

---
+++ Line.c
@@ -0,0 +1,35 @@
+#include<GL/glut.h>
+#include<GL/gl.h>
+
+void init(void)
+{
+  glClearColor(1.0,1.0,1.0,1.0);
+  glMatrixMode(GL_PROJECTION);
+  gluOrtho2D(-100.0,100,-100.0,100);
+}
+
+void setLine(void)
+{
+  int x1=0,y1=0,x,y,r=20,i;
+  glClear(GL_COLOR_BUFFER_BIT);
+  glLineWidth(3.0);
+  glBegin(GL_LINES);
+  glColor3f(1.0,0.0,0.0);
+  glVertex2i(2,2);
+  glVertex2i(9,9);
+  glEnd();
+  glFlush();
+}
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitWindowPosition(20,20);
+  glutInitWindowSize(500,500);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutCreateWindow("LINE");
+  init();
+  glutDisplayFunc(setLine);
+  glutMainLoop();
+  return 0:
+}
